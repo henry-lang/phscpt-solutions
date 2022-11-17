4a52a9e8a0da192538f92621b89bd72a2b941c17
henry-lang <henry.langmack@gmail.com>
2022-11-16 23:33:15 -0800

    soccer match

--- python/bread_first_search.py
+++ python/bread_first_search.py
@@ -15,19 +15,19 @@ for _ in range(e):
     graph[a].append(b)
     graph[b].append(a)
 
-# bfs (pos, bread, visited, depth)
-q = deque([(0, 0, set([0]), 0)])
+# bfs (pos, bread, visited)
+q = deque([(0, 0, set([0]))])
 
 while q:
     state = q.popleft()
     for other in graph[state[0]]:
         if state[1] >= n:
-            print(state[3])
+            print(len(state[2]))
             exit()
         if other not in state[2]:
             new_state = set(state[2])
             new_state.add(state[0])
 
-            q.append((other, state[1] + bread[other], new_state, state[3] + 1))
+            q.append((other, state[1] + bread[other], new_state))
 
 print(-1)
---
+++ python/soccer_match.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def read_ints():
+    return map(int, input().split())
+
+n, m = read_ints()
+grid = []
+for _ in range(n):
+    grid.append(list(input()))
+
+start = None
+for (y, row) in enumerate(grid):
+    for(x, char) in enumerate(row):
+        if char == 'S':
+            start = (y, x)
+
+# bfs (pos, visited)
+q = deque([(start, set([start]))])
+while q:
+    state = q.popleft()
+    moves = []
+    dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for i in range(4):
+        move = state[0][:] # clone it
+        while True:
+            move = tuple(map(lambda x: x[0] + x[1], zip(move, dirs[i])))
+            if move[0] < 0 or move[0] >= n or move[1] < 0 or move[1] >= m:
+                break
+            g = grid[move[0]][move[1]]
+            if g == 'X':
+                break
+            if g == 'P':
+                if move in state[1]:
+                    break
+                moves.append(move)
+                break
+            if g == 'G':
+                print(len(state[1]) + 1)
+                exit()
+    for move in moves:
+        new_visited = set(state[1])
+        new_visited.add(state[0])
+        q.append((move, new_visited))
+print(-1)
